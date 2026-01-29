2ba34ad80acd59833053012a5f3da9c1b6bed926
Gabriela <gsanchezespinoza0112@gmail.com>
2026-01-29 20:55:29 +0100

    added Day 3: How strings work

--- Day 1/Print.py
+++ Day 1/Print.py
@@ -5,7 +5,7 @@ print("My first project!")
 name = input("Please, enter your name: ")
 
 #Now we print "Hello (name)" to the console
-print(f"Hello, {name}! Welcome to the world of programming!")
+print(f"\nHello, {name}! Welcome to the world of programming!")
 
 #What's the f that you see in the print function?
 #The "f" before the string indicates that it is a formatted string literal (f-string).
@@ -13,9 +13,14 @@ print(f"Hello, {name}! Welcome to the world of programming!")
 #Another example:
 
 language = "Python"
-print(f"This sentence was built with f-string: \n{name}, you are learning {language}!")
+print(f"\nThis sentence was built with f-string: \n{name}, you are learning {language}!")
 
 
-# Curious fact: In python, you don't need to write the line break character "\n" to create a new line in the console.
-# The print function automatically adds a new line at the end of each call.
-# But, if you want a new line in the middle of your text, you can use "\n" as shown above.
+#Curious fact: In python, you don't need to write the line break character "\n" to create a new line in the console.
+#The print function automatically adds a new line at the end of each call.
+#But, if you want a new line in the middle of your text, you can use "\n" as shown above.
+
+
+
+# What you cannot do with print function:
+print("This will cause an error" + 5)  # This will raise a TypeError because you cannot concatenate a string and an integer directly.
---
+++ Day 3/Strings.py
@@ -0,0 +1,48 @@
+# Strings in Python
+# Demonstrating various string operations and methods
+
+# What is a string?
+new_string = "Hello, World!" 
+print(f"Original string: {new_string}")
+# In python, strings are sequences of characters enclosed in single (' ') or double (" ") quotes
+# These are complex objects that come with many built-in methods to manipulate and work with text data
+
+# The indexing of strings starts at 0 which is similiar to other programming languages like C, Java, and JavaScript
+# But Python also supports negative indexing, where -1 refers to the last character, -2 to the second last, and so on.
+
+print('\n-------- Indexing --------')
+
+print(new_string[0]) #This is 'H'
+print(new_string[-1]) #This is '!'
+
+print('-------- Slicing --------')
+# String slicing allows us to extract a substring from a string using the syntax [start:end]
+new_substring = new_string[0:5] 
+# This will get characters from index 0 to 4. The slicing is exclusive of the end index, so 5 is not included.
+print(new_substring) #This is 'Hello'
+
+# Slicing can also omit the start or end index to go from the beginning or to the end of the string
+print(new_string[:5])  #This is 'Hello'
+print(new_string[7:])  #This is 'World!'
+
+# Curiosity: slicing can also include a step value [start:end:step]
+every_second_char = new_string[0:13:2]
+print(every_second_char) #This is 'Hlo ol!'
+
+
+# We're going to do some practical exercises with strings
+print('\n-------- Practical Exercises --------')
+raw_string = "  Price: $49.99  "
+print(f"Raw string data: '{raw_string}'")
+
+# We can clean the string step by step or chain the methods together
+
+step_1 = raw_string.strip()  # Remove leading and trailing whitespace ' '
+step_2 = step_1.replace("$", "")  # Remove the '$' symbol
+clean_string = step_2.lower()  # Convert to lowercase for uniformity
+
+print(f"Cleaned string data: '{clean_string}'")
+
+# Now we're doing the same in one line
+chained_clean_string = raw_string.strip().replace("$", "").lower()
+print(f"Chained cleaned string data: '{chained_clean_string}'")
